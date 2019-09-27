3976c417696872eb9132c7a56d18fb0b1ff796dc
anwar1938anwar <55034228+anwar1938anwar@users.noreply.github.com>
2019-09-27 20:51:20 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("626737836011290637")
+setInterval(function() {
+channel.send(`#rep @Anwar#2279 `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
